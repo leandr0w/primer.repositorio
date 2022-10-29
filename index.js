bd8e7500b3cc2cfcd1bee96cf99c6082dfaf3dbd
Leandro Wasinger <leowasingerz@gmail.com>
2022-10-29 07:37:37 -0300

    Eliminandojs

--- index.js
+++
@@ -1,6 +0,0 @@
-const navBtn = document.querySelector(".nav-btn")
-const navMenu = document.querySelector(".-nav")
-
-navBtn.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-visible")
-})
